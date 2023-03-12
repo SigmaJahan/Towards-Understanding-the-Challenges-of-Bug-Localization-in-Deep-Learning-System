04c266a7adedf79885e94cae84e8d92b4532096a
Sigma Jahan <sigma.jahan1@gmail.com>
2023-03-12 16:27:21 -0300

    feat(bug-localization) Added the base code for BugLocator, and BLUiR for RQ1

---
+++ IRBL_for_DLSW-main/1_dataset_generation_new/f_file_mth_generator.py
@@ -0,0 +1,325 @@
+import os
+import json
+import datetime
+import shutil
+import subprocess
+import ast
+
+import os
+import json
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import humps
+import string
+import re
+from camelsplit import camelsplit
+
+stop_words = set(stopwords.words("english"))
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+def remove_numbers(text):
+    result = re.sub(r'\d+', '', text)
+    return result
+
+def remove_punctuation(text):    
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
+
+def remove_whitespace(text):
+    return  " ".join(text.split())
+
+def remove_stopwords(text):
+    word_tokens = word_tokenize(text)
+    filtered_text = [word for word in word_tokens if word not in stop_words]
+    filtered_text = [word for word in filtered_text if len(word) > 2]
+    return ' '.join(filtered_text)
+
+def convert_special_tags(text):
+    text = text.replace("&lt;", "<")
+    text = text.replace("&gt;", ">")
+    text = text.replace("&quot;", "\"")
+    text = text.replace("&#39;", "\'")
+    return text
+
+def preprocess(text):
+    text = convert_special_tags(text)
+    text = cleanhtml(text)
+    text = re.sub("[?|<|>|?|:|/|-|.|,|!|(|)|{|}|=|'|\"]", " ", text)
+    text = remove_numbers(text)
+    text = remove_whitespace(text)
+    tokens = text.split()
+    new_text = ""
+    for token in tokens:
+        if len(token) < 3:
+            continue
+        token = humps.camelize(token)
+        new_text = new_text + token+" "
+    text = new_text
+    text = remove_numbers(text)
+    text = remove_punctuation(text)
+    text = remove_whitespace(text)
+    new_text = ""
+    for tokens in text.split():
+        token = ' '.join(camelsplit(tokens))
+        new_text = new_text + token+" "
+        if token != tokens:
+            new_text = new_text + tokens+" "
+    text = remove_whitespace(new_text)
+    text = text.lower()
+    text = remove_stopwords(text)
+    return text
+    
+
+def search(dirname):
+    try:
+        filenames = os.listdir(dirname)
+        for filename in filenames:
+            full_filename = os.path.join(dirname, filename)
+            if os.path.isdir(full_filename):
+                search(full_filename)
+            else:
+                ext = os.path.splitext(full_filename)[-1]
+                file_list.append(full_filename)
+    except PermissionError:
+        pass
+
+
+def hack_methods(codes, target_file_path, encoding_type):
+    root = None
+    try:
+        data = open(target_file_path).read()
+        root = ast.parse(data, target_file_path)
+    except (UnicodeDecodeError, ValueError) as e:          
+        data = open(target_file_path, encoding=encoding_type).read()
+        root = ast.parse(data, target_file_path)
+
+
+    for node in ast.walk(root):
+        if isinstance(node, ast.FunctionDef):
+            body_text = ""
+            for body in node.body:
+                start_line =body.lineno-1
+                end_line =  body.end_lineno
+                body_text = body_text + " "+' '.join(codes[start_line:end_line])
+            yield node.name+"\t"+body_text
+
+def hack_classes(codes, target_file_path, encoding_type):
+    root = None
+    try:
+        data = open(target_file_path).read()
+        root = ast.parse(data, target_file_path)
+    except (UnicodeDecodeError, ValueError) as e:          
+        data = open(target_file_path, encoding=encoding_type).read()
+        root = ast.parse(data, target_file_path)
+
+    for node in ast.walk(root):
+        if isinstance(node, ast.ClassDef):
+            class_text = ""
+            class_text = class_text + "class " + node.name
+            body_text = ""
+            for body in node.body:
+                if isinstance(body, ast.FunctionDef):
+                    start_line = body.lineno-1
+                    end_line = body.end_lineno
+                    body_text = body_text + " " + ' '.join(codes[start_line:end_line])
+            yield [node.name, class_text + ":\n" + body_text]
+
+def hack_variables(target_file_path, encoding_type):
+    root = None
+    try:
+        data = open(target_file_path).read()
+        root = ast.parse(data, target_file_path)
+    except (UnicodeDecodeError, ValueError) as e:          
+        data = open(target_file_path, encoding=encoding_type).read()
+        root = ast.parse(data, target_file_path)
+
+    for node in ast.walk(root):
+        if isinstance(node, ast.Assign):
+            var_names = []
+            for target in node.targets:
+                if isinstance(target, ast.Name):
+                    var_names.append(target.id)
+            if var_names:
+                var_value = ast.unparse(node.value).strip()
+                yield f"{', '.join(var_names)} = {var_value}"
+
+def hack_comments(target_file_path, encoding_type):
+    root = None
+    try:
+        data = open(target_file_path).read()
+        root = ast.parse(data, target_file_path)
+    except (UnicodeDecodeError, ValueError) as e:          
+        data = open(target_file_path, encoding=encoding_type).read()
+        root = ast.parse(data, target_file_path)
+        
+    for node in ast.walk(root):
+        if isinstance(node, ast.Expr) and isinstance(node.value, ast.Str):
+            yield node.value.s
+
+def get_code_text(target_file_path):
+    codes = []
+    encoding_type = "cp949"
+    try:
+        codes = open(target_file_path, "r", encoding="utf8").readlines()
+        encoding_type ="utf8"
+    except UnicodeDecodeError:              
+        try:
+            codes = open(target_file_path, "r", encoding="euc-kr").readlines()
+            encoding_type ="euc-kr"
+        except UnicodeDecodeError:
+            try:
+                codes = open(target_file_path, "r", encoding="iso-8859-1").readlines()                              
+                encoding_type ="iso-8859-1"
+            except UnicodeDecodeError:
+                codes = open(target_file_path, "r", encoding="cp949").readlines()
+                encoding_type ="cp949"
+    return codes, encoding_type
+
+def loader():
+    path_dict = {}
+    lines = open("./z_config.txt","r", encoding="utf8").readlines()
+    for line in lines:
+        line = line.replace("\n","")
+        tokens = line.split("=",2)
+        label = tokens[0]
+        path = tokens[1]
+        path_dict[label] = path
+    return path_dict
+
+path_dict = loader()
+
+base_path = path_dict["searchspace_path"]
+file_out_path = path_dict["file_path"]
+function_out_path = path_dict["function_path"]
+class_path = path_dict["class_path"]
+variable_path = path_dict["variable_path"]
+comment_path = path_dict["comment_path"]
+
+repos = os.listdir(base_path)
+file_list = []
+for repo in repos:
+    vers = os.listdir(base_path+repo+"\\")
+    for ver in vers:
+        if os.path.exists(file_out_path+repo+"\\"+ver+"\\python\\") is True:
+            continue
+        prefix = base_path + repo+"\\"+ver+"\\"
+        space_path = base_path + repo+"\\"+ver+"\\"
+        file_list = []
+        search(space_path)
+        print(repo, ver, len(file_list))
+ 
+        if os.path.exists(file_out_path+repo+"\\"+ver+"\\python\\") is False:
+            os.makedirs(file_out_path+repo+"\\"+ver+"\\python\\")
+            os.makedirs(file_out_path+repo+"\\"+ver+"\\python_pp\\")
+        if os.path.exists(function_out_path+repo+"\\"+ver+"\\python\\") is False:
+            os.makedirs(function_out_path+repo+"\\"+ver+"\\python\\")
+            os.makedirs(function_out_path+repo+"\\"+ver+"\\python_pp\\")
+        if os.path.exists(class_path+repo+"\\"+ver+"\\python\\") is False:
+            os.makedirs(class_path+repo+"\\"+ver+"\\python\\")
+            os.makedirs(class_path+repo+"\\"+ver+"\\python_pp\\")
+        if os.path.exists(variable_path+repo+"\\"+ver+"\\python\\") is False:
+            os.makedirs(variable_path+repo+"\\"+ver+"\\python\\")
+            os.makedirs(variable_path+repo+"\\"+ver+"\\python_pp\\")
+        if os.path.exists(comment_path+repo+"\\"+ver+"\\python\\") is False:
+            os.makedirs(comment_path+repo+"\\"+ver+"\\python\\")
+            os.makedirs(comment_path+repo+"\\"+ver+"\\python_pp\\")
+            
+        f_file = open(file_out_path+repo+"\\"+ver+"\\PythonKeyMap.txt","w",encoding="utf8")
+        f_function = open(function_out_path+repo+"\\"+ver+"\\PythonKeyMap.txt","w",encoding="utf8")
+        f_class = open(class_path+repo+"\\"+ver+"\\PythonKeyMap.txt","w",encoding="utf8")
+        f_variables = open(variable_path+repo+"\\"+ver+"\\PythonKeyMap.txt","w",encoding="utf8")
+        f_comments = open(comment_path+repo+"\\"+ver+"\\PythonKeyMap.txt","w",encoding="utf8")
+
+        index_file = 0            
+        index_function = 0
+        index_class = 0            
+        index_variable = 0
+        index_comments = 0
+        for file_name in file_list:
+            if str(file_name).endswith(".py"):
+                name = file_name.replace(prefix, "")
+                output_file = file_out_path+repo+"\\"+ver+"\\python\\"+str(index_file)+".py"
+                shutil.copyfile(file_name, output_file)
+                f_file.write(str(index_file)+"|"+name+"\n")
+
+                codes, encoding_type = get_code_text(output_file)
+
+                file_writer = open(file_out_path+repo+"\\"+ver+"\\python_pp\\"+str(index_file)+".txt", "w", encoding="utf8")                
+                file_writer.write(preprocess(' '.join(codes)))
+                file_writer.close()
+
+
+                methods = ""
+                try:
+                    methods = list(hack_methods(codes, output_file, encoding_type))
+                    classes = list(hack_classes(codes, output_file, encoding_type))
+                    variables = list(hack_variables(output_file, encoding_type))
+                    comments = list(hack_comments(output_file, encoding_type))    
+                except (SyntaxError, ValueError) as e:
+                    print('error', output_file)             
+                    index_file += 1
+                    continue
+                
+                for method in methods:
+                    method_writer = open(function_out_path+repo+"\\"+ver+"\\python\\"+str(index_function)+".txt", "w", encoding="utf8")
+                    method_name = method.split("\t")[0]
+                    method_writer.write(method_name+"\n")
+                    method_body = method.split("\t",2)[1]
+                    method_writer.write(method_body)
+                    method_writer.close()
+                    f_function.write(str(index_file)+"|"+name+"|"+str(index_function)+"|"+method_name+"\n")
+
+                    pp_text = preprocess(method_name+" "+method_body)
+                    pp_writer = open(function_out_path+repo+"\\"+ver+"\\python_pp\\"+str(index_function)+".txt", "w", encoding="utf8")
+                    pp_writer.write(pp_text)
+                    pp_writer.close()
+                    index_function += 1
+    
+                for clazs in classes:
+                    class_writer = open(class_path+repo+"\\"+ver+"\\python\\"+str(index_class)+".txt", "w", encoding="utf8")
+                    class_writer.write(clazs[1])
+                    class_writer.close()
+                    f_class.write(str(index_file)+"|"+name+"|"+str(index_class)+"|"+clazs[0]+"\n")
+
+                    pp_text = preprocess(clazs[0]+" "+clazs[1])
+                    pp_writer = open(class_path+repo+"\\"+ver+"\\python_pp\\"+str(index_class)+".txt", "w", encoding="utf8")
+                    pp_writer.write(pp_text)
+                    pp_writer.close()
+                    index_class += 1
+
+                for variable in variables:
+                    variable_writer = open(variable_path+repo+"\\"+ver+"\\python\\"+str(index_variable)+".txt", "w", encoding="utf8")
+                    variable_writer.write(variable)
+                    variable_writer.close()
+                    f_variables.write(str(index_file)+"|"+name+"|"+str(index_variable)+"|"+variable+"\n")
+
+                    pp_text = preprocess(variable)
+                    pp_writer = open(variable_path+repo+"\\"+ver+"\\python_pp\\"+str(index_variable)+".txt", "w", encoding="utf8")
+                    pp_writer.write(pp_text)
+                    pp_writer.close()
+                    index_variable += 1
+                
+                for comment in comments:
+                    comment_writer = open(comment_path+repo+"\\"+ver+"\\python\\"+str(index_comments)+".txt", "w", encoding="utf8")
+                    comment_writer.write(comment)
+                    comment_writer.close()
+                    f_comments.write(str(index_file)+"|"+name+"|"+str(index_comments)+"|"+comment+"\n")
+
+                    pp_text = preprocess(comment)
+                    pp_writer = open(comment_path+repo+"\\"+ver+"\\python_pp\\"+str(index_comments)+".txt", "w", encoding="utf8")
+                    pp_writer.write(pp_text)
+                    pp_writer.close()
+                    index_comments += 1
+                index_file += 1
+
+        f_file.close()
+        f_class.close()
+        f_function.close()
+        f_variables.close()
+        f_comments.close()
---
+++ IRBL_for_DLSW-main/1_dataset_generation_new/g2_gtf_generator_function.py
@@ -0,0 +1,262 @@
+import os
+import json
+import datetime
+
+
+def loader():
+    path_dict = {}
+    lines = open("./z_config.txt","r", encoding="utf8").readlines()
+    for line in lines:
+        line = line.replace("\n","")
+        tokens = line.split("=",2)
+        label = tokens[0]
+        path = tokens[1]
+        path_dict[label] = path
+    return path_dict
+
+path_dict = loader()
+
+
+bugs_path = path_dict["bug_path"]
+gtfs_path = path_dict["gtf_path"]
+repos = os.listdir(bugs_path)
+target_bug_num = 0
+target_repo_num = 0
+for repo in repos:
+
+    versions = os.listdir(bugs_path+repo+"\\")
+    project_bug_num = 0
+    for version in versions:
+        files_path = path_dict["file_path"]
+        methods_path = path_dict["function_path"]
+        class_path = path_dict["class_path"]
+        variable_path = path_dict["variable_path"]
+        comment_path = path_dict["comment_path"]
+        if os.path.exists(gtfs_path+repo+"\\"+version+"\\") is False:
+            os.makedirs(gtfs_path+repo+"\\"+version+"\\")
+        bugs = os.listdir(bugs_path+repo+"\\"+version+"\\")
+        files = os.listdir(methods_path+repo+"\\"+version+"\\")
+        classes = os.listdir(class_path+repo+"\\"+version+"\\")
+        variables = os.listdir(variable_path+repo+"\\"+version+"\\")
+        comments = os.listdir(comment_path+repo+"\\"+version+"\\")
+
+        files_dict = {}
+        files_dict_by_name = {}
+        files_name_set= set()
+
+        methods_dict = {}
+        methods_dict_by_name = {}
+        methods_name_set= set()
+
+        class_dict = {}
+        class_dict_by_name = {}
+        class_name_set= set()
+
+        variable_dict = {}
+        variable_dict_by_name = {}
+        variable_name_set= set()
+
+        comment_dict = {}
+        comment_dict_by_name = {}
+        comment_name_set= set()
+
+        for file in files:
+            if file.endswith(".txt") is False:
+                continue
+            lang = file.replace("KeyMap.txt", "").lower()
+
+            files_path = methods_path+repo+"\\"+version+"\\"+file
+            f = open(files_path, "r", encoding="utf8")
+            lines = f.readlines()
+            for line in lines:
+                line = line.replace("\n","")
+                if line.find("|") == -1:
+                    continue
+                tokens = line.split("|")
+                file_id = tokens[0]
+                file_name = tokens[1].lower().replace("\\\\","\\")
+                files_dict[file_id] = file_name
+                files_name_set.add(file_name)
+                files_dict_by_name[file_name] = file_id
+
+                method_id = file_id+"|"+tokens[2]
+                method_name = file_name+"|"+tokens[3].lower()
+                methods_dict[method_id] = method_name
+                methods_name_set.add(method_name)
+                methods_dict_by_name[method_name] = method_id
+        
+        for class_file in classes:
+            if class_file.endswith(".txt") is False:
+                continue
+            lang = class_file.replace("KeyMap.txt", "").lower()
+            class_path = class_path+repo+"\\"+version+"\\"+class_file
+            f = open(class_path, "r", encoding="utf8")
+            lines = f.readlines()
+            for line in lines:
+                line = line.replace("\n","")
+                if line.find("|") == -1:
+                    continue
+                tokens = line.split("|")
+                file_id = tokens[0]
+                file_name = tokens[1].lower().replace("\\\\","\\")
+                files_dict[file_id] = file_name
+                files_name_set.add(file_name)
+                files_dict_by_name[file_name] = file_id
+
+                class_id = file_id+"|"+tokens[2]
+                class_name = file_name+"|"+tokens[3].lower()
+                class_dict[class_id] = class_name
+                class_name_set.add(class_name)
+                class_dict_by_name[class_name] = class_id
+        
+        for variable_file in variables:
+            if variable_file.endswith(".txt") is False:
+                continue
+            lang = variable_file.replace("KeyMap.txt", "").lower()
+            variable_path = variable_path+repo+"\\"+version+"\\"+variable_file
+            f = open(variable_path, "r", encoding="utf8")
+            lines = f.readlines()
+            for line in lines:
+                line = line.replace("\n","")
+                if line.find("|") == -1:
+                    continue
+                tokens = line.split("|")
+                file_id = tokens[0]
+                file_name = tokens[1].lower().replace("\\\\","\\")
+                files_dict[file_id] = file_name
+                files_name_set.add(file_name)
+                files_dict_by_name[file_name] = file_id
+
+                variable_id = file_id+"|"+tokens[2]
+                variable_name = file_name+"|"+tokens[3].lower()
+                variable_dict[variable_id] = variable_name
+                variable_name_set.add(variable_name)
+                variable_dict_by_name[variable_name] = variable_id
+        
+        for comment_file in comments:
+            if comment_file.endswith(".txt") is False:
+                continue
+            lang = comment_file.replace("KeyMap.txt", "").lower()
+            comment_path = comment_path+repo+"\\"+version+"\\"+comment_file
+            f = open(comment_path, "r", encoding="utf8")
+            lines = f.readlines()
+            for line in lines:
+                line = line.replace("\n","")
+                if line.find("|") == -1:
+                    continue
+                tokens = line.split("|")
+                file_id = tokens[0]
+                file_name = tokens[1].lower().replace("\\\\","\\")
+                files_dict[file_id] = file_name
+                files_name_set.add(file_name)
+                files_dict_by_name[file_name] = file_id
+
+                if len(tokens) <= 2:
+                    continue
+                comment_id = file_id+"|"+tokens[2]
+                comment_name = file_name+"|"+tokens[3].lower()
+                comment_dict[comment_id] = comment_name
+                comment_name_set.add(comment_name)
+                comment_dict_by_name[comment_name] = comment_id
+        
+        for bug in bugs:                      
+            bug_path = bugs_path+repo+"\\"+version+"\\"+bug
+            contents = None
+            with open(bug_path, 'r', encoding ="utf8") as j:
+                contents = json.loads(j.read())
+            
+            bug_id = bug.replace(".json","")
+            changed_files = contents['commit']['changed_files']
+            file_type_set = set()            
+            buggy_methods_ids = set()
+            buggy_classes_ids = set()
+            buggy_variables_ids = set()
+            buggy_comments_ids = set()
+            for changed_file in changed_files:
+
+                change_type = contents['commit']['changed_files'][changed_file]['file_change_type']
+                if change_type != "MODIFY":
+                    continue
+                original_file_name = contents['commit']['changed_files'][changed_file]['file_old_name'].lower()
+                new_file_name = contents['commit']['changed_files'][changed_file]['file_new_name'].lower()
+
+                if original_file_name not in files_name_set:
+                    continue
+                hunks =  contents['commit']['changed_files'][changed_file]["hunks"]
+                for hunk in hunks:
+                    isMethod = contents['commit']['changed_files'][changed_file]["hunks"][hunk]["Ismethod"]
+                    if isMethod == 1:
+                        method_name = contents['commit']['changed_files'][changed_file]["hunks"][hunk]["method_info"]["method_name"].lower()
+                        method_identifier = original_file_name+"|"+method_name        
+                        if method_identifier in methods_name_set:
+                            buggy_methods_ids.add(methods_dict_by_name[method_identifier].split("|")[1])
+
+                # Iterate over the files_dict
+                for file_id in files_dict:
+                    if files_dict[file_id] == original_file_name:
+                        file_index = file_id
+                        break
+                    if files_dict[file_id] == new_file_name:
+                        file_index = file_id
+                        break
+
+                # Open the keymap for class
+                class_keymap_file = open (class_path, "r", encoding="utf8")
+                lines = class_keymap_file.readlines()
+                for line in lines:
+                    line = line.replace("\n", "")
+                    if line.find("|") == -1:
+                        continue
+                    tokens = line.split("|")
+                    if tokens[0] == file_index:
+                        buggy_classes_ids.add(tokens[-2])
+                class_keymap_file.close()
+
+                # Open the keymap for variable
+                variable_keymap_file = open (variable_path, "r", encoding="utf8")
+                lines = variable_keymap_file.readlines()
+                for line in lines:
+                    line = line.replace("\n", "")
+                    if line.find("|") == -1:
+                        continue
+                    tokens = line.split("|")
+                    if tokens[0] == file_index:
+                        buggy_variables_ids.add(tokens[-2])
+                variable_keymap_file.close()
+
+                # Open the keymap for comment
+                comment_keymap_file = open (comment_path, "r", encoding="utf8")
+                lines = comment_keymap_file.readlines()
+                for line in lines:
+                    line = line.replace("\n", "")
+                    if line.find("|") == -1:
+                        continue
+                    tokens = line.split("|")
+                    if tokens[0] == file_index:
+                        buggy_comments_ids.add(tokens[-2])
+                comment_keymap_file.close()
+
+            print(bug_id, buggy_methods_ids)
+            print(repo, version, bug, buggy_methods_ids)
+            if len(buggy_methods_ids) == 0:
+                continue
+            f = open(gtfs_path+repo+"\\"+version+"\\"+bug_id+"_method.txt", "w", encoding="utf8")
+            for buggy_mth_id in buggy_methods_ids:
+                f.write("python-"+buggy_mth_id+"\n")
+            f = open(gtfs_path+repo+"\\"+version+"\\"+bug_id+"_class.txt", "w", encoding="utf8")
+            for buggy_cls_id in buggy_classes_ids:
+                f.write("python-"+buggy_cls_id+"\n")
+            f = open(gtfs_path+repo+"\\"+version+"\\"+bug_id+"_variable.txt", "w", encoding="utf8")
+            for buggy_var_id in buggy_variables_ids:
+                f.write("python-"+buggy_var_id+"\n")
+            f = open(gtfs_path+repo+"\\"+version+"\\"+bug_id+"_comment.txt", "w", encoding="utf8")
+            for buggy_cmt_id in buggy_comments_ids:
+                f.write("python-"+buggy_cmt_id+"\n")
+            f.close()
+            target_bug_num += 1
+            project_bug_num += 1
+    if project_bug_num > 0:
+        target_repo_num += 1
+print(target_repo_num, target_bug_num,project_bug_num)
+
+
